b8233154aeaf42e4159b9c97afa45715616ccb07
Had612 <hadyAlmahfoudh@gmail.com>
2024-11-13 21:10:09 +0300

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,1 +1,33 @@
-
+public class Course {
+    //Attributes
+    private String title;
+    private String instructor;
+    private String area;
+    //Getters
+    public String getTitle {
+      return title;
+    }
+    public String grtInstructor {
+      return instructor;
+    }
+    public String getArea {
+      return area;
+    }
+    //Setters
+    public void setTitle {
+      this.title = title;
+     }
+    public void setInstructor {
+      this.instructor = instructor; 
+    }
+    public void 
+    setArea(String area) {
+       this.area = area;
+    //(Opional) Constructor - initializes attributes during object creation
+    public Course(String title, String instructor , String area) {
+        this.title = title;
+        this.instructor = instructor;
+        this.area = area; 
+       }
+    }
+    
