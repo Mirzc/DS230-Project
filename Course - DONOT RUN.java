27fbaecadeb63743ce5bec7b5897fb010f5a29ab
Mirzc <131402238+Mirzc@users.noreply.github.com>
2024-11-16 15:41:17 +0300

    updates

---
+++ Course - DONOT RUN.java
@@ -0,0 +1,39 @@
+public class Course {
+  // Attributes
+  private String title;
+  private String instructor;
+  private String area;
+
+  // Getters
+  public String getTitle() {
+    return title;
+  }
+
+  public String getInstructor() {
+    return instructor;
+  }
+
+  public String getArea() {
+    return area;
+  }
+
+  // Setters
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public void setInstructor(String instructor) {
+    this.instructor = instructor;
+  }
+
+  public void setArea(String area) {
+    this.area = area;
+  }
+
+  // (Optional) Constructor - initializes attributes during object creation
+  public Course(String title, String instructor, String area) {
+    this.title = title;
+    this.instructor = instructor;
+    this.area = area;
+  }
+}
