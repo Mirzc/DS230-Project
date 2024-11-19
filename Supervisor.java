f7b166d9b2341c3e2a3a4b1c32d5ee52a7805c78
Mirzc <131402238+Mirzc@users.noreply.github.com>
2024-11-19 19:04:44 +0300

    Update Supervisor.java

--- Supervisor.java
+++ Supervisor.java
@@ -1,29 +1,28 @@
 class Supervisor {
-    public List<Classes> subjects;
+    public List<Course> title;
 
     public Supervisor() {
-        subjects = new ArrayList<>();
-        subjects.add(new Classes("Intro to Data Science", "Dr. Mohammed", "Dammam"));
-        subjects.add(new Classes("Islam 101", "Dr. Ahmed", "Dammam"));
-        subjects.add(new Classes("Mathamatcis: Algebra", "Dr. Khalid", "Riyadh"));
-        subjects.add(new Classes("Ethics for Data Science", "Dr. Ali", "Jeddah"));
+        title = new ArrayList<>();
+        title.add(new Course("Intro to Data Science", "Dr. Mohammed", "Dammam"));
+        title.add(new Course("Islam 101", "Dr. Ahmed", "Dammam"));
+        title.add(new Course("Mathamatcis: Algebra", "Dr. Khalid", "Riyadh"));
+        title.add(new Course("Ethics for Data Science", "Dr. Ali", "Jeddah"));
     }
 
-    public void displayAllSubjects() {
-        System.out.println("Available Subjects:");
-        for (int i = 0; i < subjects.size(); i++) {
-            System.out.println((i + 1) + ". " + subjects.get(i));
-                        }
-            }
+    public void displayAlltitle() {
+        System.out.println("Available title:");
+        for (int i = 0; i < title.size(); i++) {
+            System.out.println((i + 1) + ". " + title.get(i));
+        }
+    }
 
-    public void searchSubjects(String phrase)   
-    {
+    public void searchtitle(String phrase) {
         System.out.println("Search Results:");
         boolean notFound = false;
-        for (Classes subject : subjects) {
-            if (subject.subjectTitle.equalsIgnoreCase(phrase) || subject.subjectInstructor.equalsIgnoreCase(phrase))
-            {                   System.out.println(subject);
-                                        notFound = true;
+        for (Course course : title) {
+            if (course.title.equalsIgnoreCase(phrase) || course.instructor.equalsIgnoreCase(phrase)) {
+                System.out.println(course);
+                notFound = true;
             }
         }
         if (!notFound) {
@@ -31,31 +30,38 @@ class Supervisor {
         }
     }
 
-    public void displaySubjectsByarea(String area) {
-        System.out.println("Subjects in " + area + ":");
+    public void displaytitleByarea(String area) {
+        System.out.println("title in " + area + ":");
         boolean notFound = false;
-        for (Classes subject : subjects) {
+        for (Course subject : title) {
             if (subject.area.equalsIgnoreCase(area)) {
                 System.out.println(subject);
                 notFound = true;
             }
         }
         if (!notFound) {
-            System.out.println("No subjects notFound in: " + area);
+            System.out.println("No title notFound in: " + area);
         }
     }
 
-   public void displaySubjectsBySubjectInstructor(String subjectInstructor) {
-        System.out.println("Subjects in " + subjectInstructor + ":");
+    public void displaytitleBySubjectInstructor(String subjectInstructor) {
+        System.out.println("title in " + subjectInstructor + ":");
         boolean notFound = false;
-        for (Classes subject : subjects) {
+        for (Course subject : title) {
             if (subject.area.equalsIgnoreCase(subjectInstructor)) {
                 System.out.println(subject);
                 notFound = true;
             }
         }
         if (!notFound) {
-            System.out.println("No subjects notFound in: " + subjectInstructor);
+            System.out.println("No title notFound in: " + subjectInstructor);
         }
     }
+
+    public void addCourse(String title, String instructor, String area) {
+        Course newCourse = new Course(title, instructor, area);
+        this.title.add(newCourse);
+        System.out.println("The Course has been " + newCourse.title + " added: ");
+    }
+
 }
